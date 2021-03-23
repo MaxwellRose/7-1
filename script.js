5d617697a868c1721158a96e1f9bee33fb5b8fae
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-23 13:43:28 +0000

    some errors

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ let userguess = 0
 document.getElementById('correct').addEventListener('click', guess)
 
 function guess () {
- userguess = document.getElementById('num')
- .value
- userguess=parseInt(userguess)
+  userguess = document.getElementById('num').value
+  userguess=parseInt(userguess)
  
  randomnumber = Math.random() * 6 + 1
- randomnumber=parseInt(randomnumber) 
+ randomnumber = parseInt(randomnumber) 
 
- if (userguess == randomnumber) {
-   document.getElementById('p').innerHTML = "you win!"
- }
+ if (userguess === randomnumber) {
+    document.getElementById('p').innerHTML = "you win!"
+  }
 }
