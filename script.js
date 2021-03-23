cfb6d2d3d9c213126f76a2b2ff1c7e3c5f68bcbe
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-23 13:29:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let randomnumber = 0
+let userguess = 0
+
+document.getElementById('correct').addEventListener('click', guess)
+
+function guess () {
+ userguess = document.getElementById('num')
+ .value
+ userguess=parseInt(userguess)
+ 
+ randomnumber = Math.random() * 6 + 1
+ randomnumber=parseInt(randomnumber) 
+
+ if (userguess == randomnumber) {
+   document.getElementById('p').innerHTML = "you win!"
+ }
+}
